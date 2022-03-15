cf30d3bde5078a2ab6373cdc4132885de0af22bf
ALLOJU <mounika.alloju@gmail.com>
2022-03-15 15:48:50 +0530

    UC3 with edit contacts added

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class AddressBook {
@@ -21,8 +22,9 @@ public class AddressBook {
 	/**
 	 * 2.to add person details into array list
 	 */
+	Scanner s=new Scanner(System.in);
+
 	public void addPerson() {
-		Scanner s=new Scanner(System.in);
 		Persons person = new Persons();
 		System.out.println("Enter the First name:");
 		String fname = s.next();
@@ -40,21 +42,21 @@ public class AddressBook {
 		System.out.println("Enter the City:");
 		String city = s.next();
 		person.setCity(city);
-		
+
 		System.out.println("Enter the State:");
 		String state = s.next();
 		person.setState(state);
-		
+
 		System.out.println("Enter the Zip:");
 		String zip = s.next();
 		person.setZip(zip);
 
-		
-		
+
+
 		System.out.println("Enter the Phone :");
 		Long phone = s.nextLong();
 		person.setPhone_number(phone);
-		
+
 		System.out.println("Enter the Email :");
 		String email = s.next();
 		person.setEmail(email);
@@ -70,6 +72,55 @@ public class AddressBook {
 					+person.getCity()+"\t"+person.getState()+ "\t"+person.getZip()+"\t"+person.getPhone_number()+"\t"+person.getEmail());
 
 	}
+	public void editPerson() {
+		System.out.println("Enter your First name:");
+		String fname = s.next();
+		Iterator<Persons> iterator = persons.listIterator();
+		while (iterator.hasNext()) {
+			Persons person = iterator.next();
+			if (fname.equals(person.getFirst_name())) {
+
+				System.out.println("Enter Last Name");
+				String last_name=s.next();
+				//p.setFirst_name(first_name);
+				//p.setLast_name(last_name);
+
+
+				System.out.println("Enter Address");
+				String address=s.next();
+				//p.setAddress(address);
+
+				System.out.println("Enter City");
+				String city=s.next();
+				//p.setCity(city);
+
+				System.out.println("Enter State");
+				String state=s.next();
+				//p.setState(state);
+
+				System.out.println("Enter Zip code");
+				String zip=s.next();
+				//p.setZip(zip);
+
+				System.out.println("Enter Phone Number");
+				long phone_number=s.nextLong();
+				//p.setPhone_number(phone_number);
+
+				System.out.println("Enter email");
+				String email=s.next();
+
+
+				person.setLast_name(last_name);
+				person.setAddress(address);
+				person.setCity(city);
+				person.setState(state);
+				person.setZip(zip);
+				person.setPhone_number(phone_number);
+				person.setEmail(email);
+			}
+		}
+	}
+
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book System ");
@@ -77,6 +128,7 @@ public class AddressBook {
 
 		System.out.println("Choose option to perform action");
 		System.out.println("Enter 1 to add person details");
+		System.out.println("Enter 2 to Edit person details");
 		Scanner sc=new Scanner(System.in);
 		int num=sc.nextInt();
 		char choice;
@@ -87,7 +139,11 @@ public class AddressBook {
 				ab.addPerson();
 				ab.display();
 				break;
-
+			case 2:
+				
+				ab.editPerson();
+				ab.display();
+				break;	
 
 			}
 			System.out.println("Do You Want To Continue(Y/N)");
@@ -99,6 +155,7 @@ public class AddressBook {
 		sc.close();
 
 	}
+
 
 
 }
