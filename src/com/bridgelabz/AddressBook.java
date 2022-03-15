7e0a596e5552e0fdc843dc12e3baf71d6b357fa0
ALLOJU <mounika.alloju@gmail.com>
2022-03-15 15:03:43 +0530

    address book with contacts created

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,16 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+
+	ArrayList<Persons> persons;
+	public AddressBook() {
+		persons=new ArrayList<Persons>();
+		
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book System ");
+	}
+}
